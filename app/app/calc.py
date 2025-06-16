810c0f75b363004f5a4b62d981fdc8650cf0336d
Mark Romero <mark.romero@energiasolarsa.com>
2025-06-16 10:01:58 -0500

    db connection

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,10 @@ Calculator functions
 
 def add(x, y):
     """Add c and y and return result."""
+
     return x + y
 
 def subtract(x, y):
     """Add c and y and return result."""
+
     return x - y
-
-
