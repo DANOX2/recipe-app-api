d4a6f43e793e89b210340b78b7d6443db7ebbcda
Mark Romero <mark.romero@energiasolarsa.com>
2025-06-26 17:19:01 -0500

    update tags tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,12 +5,14 @@ from rest_framework import (
     viewsets,
     mixins,
 )
+
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
     Tag,
+)
 from recipe import serializers
 
 
@@ -36,12 +38,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
-    queryset = Tag.object.all()
-    authentication_class = [TokenAuthentication]
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
